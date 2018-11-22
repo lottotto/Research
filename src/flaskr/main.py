709512f9a65e083614c52ec2b053f390a11a1fe4
Kentaro Saito <saito.k.bx@m.titech.ac.jp>
2018-11-22 20:43:32 +0900

    Web雛形完成

--- src/flaskr/main.py
+++ src/flaskr/main.py
@@ -1,40 +1,76 @@
+import os
 from flask import Flask, current_app, request, flash,redirect,url_for,render_template
 from flask_pymongo import PyMongo
 from flask_bootstrap import Bootstrap
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['MONGO_URI'] = "mongodb://localhost:27017/shelter"
-app.config['MONGO_HOST'] = "localhost"
-app.config['MONGO_PORT'] = 27017
-app.config['MONGO_DBNAME'] = "shelter"
 mongo = PyMongo(app)
+line_mongo = PyMongo(app, uri="mongodb://localhost:27017/LINE")
+line_bot_api = LineBotApi(os.environ['LINE_MESSAGE_API_ACCESS_TOKEN'])
 
 
-def make_index_recode(mongo_document):
-    ret_dict = {}
-    ret_dict['code'] = mongo_document['code']
-    ret_dict['datetime'] = mongo_document['datetime'].strftime("%Y年%m月%d日 %H時%M分")
-    temperature = mongo_document['sensor']['tmp']
-    ret_dict['sensor_tmp'] = temperature
-    humidity = mongo_document['sensor']['hum']
-    ret_dict['sensor_hum'] = humidity
-    ret_dict['sensor_lux'] = mongo_document['sensor']['lux']
-    ret_dict['sensor_co2'] = mongo_document['sensor']['co2']
-    if 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3 > 85:
-        ret_dict['remark'] = '【危険】不快指数 85以上！'
-    return ret_dict
+def send_LINE(user_id, message):
+    try:
+        line_bot_api.push_message(user_id, text=TextSendMessage(text=message))
+    except LineBotApiError as e:
+        pass
+
+# def make_index_recode(mongo_document):
+#     ret_dict = {}
+#     ret_dict['code'] = mongo_document['code']
+#     ret_dict['sensor_time'] = mongo_document['sensor_time'].strftime("%Y%m%d-%H:%M")
+#     ret_dict['sensor_tmp'] = mongo_document['sensor']['tmp']
+#     ret_dict['sensor_hum'] = mongo_document['sensor']['hum']
+#     ret_dict['sensor_lux'] = mongo_document['sensor']['lux']
+#     ret_dict['sensor_co2'] = mongo_document['sensor']['co2']
+#     try:
+#         ret_dict['name'] = mongo_document['name']
+#     except:
+#         pass
+#     tmp = ret_dict['sensor_tmp']
+#     hum = ret_dict['sensor_hum']
+#     if 0.81 * tmp + 0.01 * hum * (0.99 * tmp - 14.3) + 46.3 > 85:
+#         ret_dict['remark'] = '【危険】不快指数 85以上！'
+#     return ret_dict
+
+def calc_remark(mongo_document):
+    tmp = mongo_document['sensor']['tmp']
+    if tmp > 38:
+        ret_str = "アツゥイ！"
+    else:
+        ret_str ="特になし"
+    return ret_str
+
+def make_line_entries(collection_name):
+    print(line_mongo.db[collection_name])
+    return [document for document in line_mongo.db[collection_name].find()]
+
+
+@app.route('/<db_name>/<collection_name>', methods=['GET','POST'])
+def show_entries(db_name='', collection_name=''):
+    app.config['MONGO_URI'] = "mongodb://localhost:27017/"+db_name
+    mongo.init_app(app)
+    shelters = mongo.db[collection_name].find()
+    entries = []
+    for document in shelters:
+        document['remark'] = calc_remark(document)
+        entries.append(document)
+    Line_entries = make_line_entries(collection_name)
+    if request.method == 'POST':
+        send_LINE(user_id=request.form['user_id'], message=request.form['message'])
+
+    return render_template('new_new_index.html', entries=entries, Line_entries=Line_entries)
+
 
 
 @app.route('/', methods=['GET', "POST"])
-def show_entries():
-    shelters = mongo.db.shelter.find()
-    entries = []
-    for document in shelters:
-        temp_dict = make_index_recode(document)
-        entries.append(temp_dict)
-
-    return render_template('index.html', entries=entries)
+def top_page():
+    return "URLにdb_nameとcollection_nameをつけてね"
 
 
 # @app.route('/post', methods=['POST'])
--- src/subscriber_Mongo.py
+++ src/subscriber_Mongo.py
@@ -10,8 +10,8 @@ mongo_client = MongoClient('localhost', 27017)
 
 
 def setting_Mongo(topic):
-    db_name = topic.split('/')[3]
-    collection_name = "_".join(topic.split('/')[1:2])
+    db_name = topic.split('/')[2] #test, training, defaultなど
+    collection_name = topic.split('/')[3]  #201801
     db = mongo_client[db_name]
     collection = db[collection_name]
     return db, collection
@@ -20,11 +20,12 @@ def insert_questionnaire_data(message, topic):
     _, collection = setting_Mongo(topic)
     json_data = json.loads(message)
     code = topic.split('/')[-1]
-    questionnaire_now = datetime.now()
+    questionnaire_now = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     json_data['code'] = code
     json_data['questionnaire_time'] = questionnaire_now
     if collection.find_one({"code":code}) is None:
         collection.insert(json_data)
+
     else:
         document = collection.find_one({"code":code})
         for key, value in json_data.items():
@@ -35,14 +36,26 @@ def insert_sensor_data(message,topic):
     _, collection = setting_Mongo(topic)
     code = topic.split('/')[-1]
     json_data = json.loads(message)
-    sensor_now = datetime.now()
+    sensor_now = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
 
     if collection.find_one({"code":code}) is None:
         collection.insert({"sensor_time":sensor_now,"code":code,"sensor":json_data})
     else:
         document = collection.find_one({"code":code})
         document['sensor'] = json_data
+        document['sensor_time'] = sensor_now
         collection.save(document)
+
+def insert_line_data(message, topic):
+    db = mongo_client['LINE']
+    collection = db[topic.split('/')[3]]
+    json_data = json.loads(message)
+    if collection.find_one({"UserID":json_data['UserID']}) is None:
+        collection.insert(json_data)
+    else:
+        document = collection.find_one({"UserID":json_data['UserID']})
+        collection.save(document)
+
 
 
 def on_connect(client, userdata, flags, respons_code):
@@ -56,12 +69,14 @@ def on_message(client, userdata, msg):
     subscribed_topic = msg.topic
     message = msg.payload.decode()
     print("{}\t{}\t{}".format(now, subscribed_topic, message))
-    code = subscribed_topic.split('\')[-1]
-    recieve_data_type = subscribed_topic.split('\')[-2]
+    code = subscribed_topic.split('/')[-1]
+    recieve_data_type = subscribed_topic.split('/')[-2]
     if recieve_data_type == 'app':
-        insert_questionnaire_data(message, code)
+        insert_questionnaire_data(message, subscribed_topic)
     elif recieve_data_type == 'sensor':
         insert_sensor_data(message,subscribed_topic)
+    elif recieve_data_type == 'line':
+        insert_line_data(message,subscribed_topic)
 
 
 if __name__ == '__main__':
