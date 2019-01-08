0d37fa2820f580411ebb366a94b1908a84f7c94d
Kentaro Saito <saito.k.bx@m.titech.ac.jp>
2019-01-08 22:22:01 +0900

    a

--- ocr/src/Img_sub.py
+++ ocr/src/Img_sub.py
@@ -12,8 +12,8 @@ host = env_data["host"]
 port = 1883
 topic =env_data["topic"]
 store_path = env_data["store_path"]
-PathToCastDefCsv = './Resource/CastDef.csv'
-PathToSheatStyle = './Resource/OCR_style.csv'
+cast_def_path = './Resource/CastDef.csv'
+style_def_path = './Resource/OCR_style.csv'
 # topic /disfor/training/18000/shelter/pic
 
 def makeStoreDir(subTopic):
@@ -32,7 +32,7 @@ def makeStoreDir(subTopic):
             continue
 
 def on_connect(client, userdata, flags, respons_code):
-    print('status {0}'.format(respons_code))
+    print('status {0}, host:{1}, topic:{2}'.format(respons_code, host, topic))
     client.subscribe(topic)
     print('Connect')
 
@@ -45,17 +45,16 @@ def on_message(client, userdata, msg):
     with open(name, "wb") as fh:
         fh.write(base64.decodebytes(msg.payload))
     print("Image Saved in Path:{}".format(name))
-    MAIN.main(srcPATH=name, StyleCsvPath=PathToSheatStyle)
-    SheatPATH = os.path.join(tempPath,"corrected.jpg")
+    MAIN.main(srcPATH=name, StyleCsvPath=style_def_path)
+    sheat_path = os.path.join(tempPath,"corrected.jpg")
     try:
-        CastResult_Publish.publish(Host=host, Topic=msg.topic, CastDefCsv=PathToCastDefCsv, SheatPATH=SheatPATH)
+        CastResult_Publish.publish(Host=host, Topic=msg.topic, CastDefCsv=cast_def_path, SheatPATH=sheat_path)
         print("{} DONE OCR".format(name))
     except:
         pass
 
 
 if __name__ == '__main__':
-    print(sys.argv)
     # Publisherと同様に v3.1.1を利用
     client = mqtt.Client(protocol=mqtt.MQTTv311)
 
